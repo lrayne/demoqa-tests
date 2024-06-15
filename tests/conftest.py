ccc87a964c47eebb75466bcfb39a69c8ee00d3d5
lrayne <legaterayne@gmail.com>
2024-06-16 00:56:05 +0300

    Modify tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,53 @@
+import os
 import pytest
+from dotenv import load_dotenv
 from selene import browser
-from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from demoqa_tests.utils import attach
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version', help='Версия браузера', default='100.0'
+    )
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_management():
+def browser_management(request):
+    browser_version = request.config.getoption('--browser_version')
+
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {"enableVNC": True, "enableVideo": True},
+    }
 
-    yield
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
+    options.capabilities.update(selenoid_capabilities)
+    browser.config.driver_remote_url = (
+        f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub"
+    )
+
+    browser.config.driver_options = options
+
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
