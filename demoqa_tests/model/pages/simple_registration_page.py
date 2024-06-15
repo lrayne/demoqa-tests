7ea07ee493fbb2b2a318b0a318b8a3cddca97c83
lrayne <legaterayne@gmail.com>
2024-06-16 00:04:56 +0300

    Add test_register_a_student_using_simplified_form()

---
+++ demoqa_tests/model/pages/simple_registration_page.py
@@ -0,0 +1,35 @@
+from selene import browser, have, command
+from selene.support.shared.jquery_style import s
+from demoqa_tests.model.components.panel import Panel
+from demoqa_tests.model.data import User
+
+
+class SimpleRegistrationPage:
+    def __init__(self):
+        self.panel = Panel()
+
+    def open(self):
+        browser.open('/text-box')
+        return self
+
+    def register(self, user: User):
+        s('#userName').type(f'{user.first_name} {user.last_name}')
+        s('#userEmail').type(user.email)
+        s('#currentAddress').type(user.address)
+        s('#permanentAddress').type(user.permanent_address)
+
+        s('#submit').perform(command.js.scroll_into_view).click()
+
+    def should_be_registered(self, user: User):
+        s('#output').s('#name').should(
+            have.exact_text(f'Name:{user.first_name} {user.last_name}')
+        )
+        s('#output').s('#email').should(have.exact_text(f'Email:{user.email}'))
+
+        s('#output').s('#currentAddress').should(
+            have.exact_text(f'Current Address :{user.address}')
+        )
+
+        s('#output').s('#permanentAddress').should(
+            have.exact_text(f'Permananet Address :{user.permanent_address}')
+        )
--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,66 +1,24 @@
-from datetime import date
-from selene import have
-from demoqa_tests.model.pages.home_page import HomePage
-from demoqa_tests.model.pages.forms_page import FormsPage
-from demoqa_tests.model.pages.registration_page import RegistrationPage
-from demoqa_tests.model.data import User, State, City, Hobby, Subject, Gender
+from demoqa_tests.app import app
+from demoqa_tests.model.data import student
 
 
 def test_register_a_student():
 
-    # GIVEN
-    home_page = HomePage()
-    forms_page = FormsPage()
-    registration_page = RegistrationPage()
-    student = User(
-        first_name='Aleksei',
-        last_name='Torsukov',
-        email='torsukov@test.ru',
-        gender=Gender.male,
-        mobile='8999123440',
-        date_of_birth=date(1998, 10, 11),
-        subjects=[Subject.english, Subject.computer_science],
-        hobbies=[Hobby.sports, Hobby.reading],
-        avatar='photo.png',
-        address='Robert Robertson, 1234 NW Bobcat Lane, St. Robert, MO 65584-5678',
-        state=State.ncr,
-        city=City.gurgaon,
-    )
+    app.open()
 
-    # WHEN
-    home_page.open()
-    home_page.select_forms()
-    forms_page.panel.select_student_registration_form()
+    app.home_page.select_forms()
+    app.forms_page.panel.select_student_registration_form()
+    app.registration_page.register(student)
 
-    (
-        registration_page.form
-        .fill_first_name(student.first_name)
-        .fill_last_name(student.last_name)
-        .fill_email(student.email)
-        .fill_gender(student.gender.male)
-        .fill_mobile(student.mobile)
-        .fill_date_of_birth(student.date_of_birth)
-        .fill_subjects(student.subjects)
-        .set_hobbies(student.hobbies)
-        .upload_avatar(student.avatar)
-        .fill_current_address(student.address)
-        .fill_state(student.state)
-        .fill_city(student.city)
-        .submit()
-    )
+    app.registration_page.should_be_registered(student)
 
-    # THEN
-    registration_page.user_data.should(
-        have.exact_texts(
-            f'{student.first_name} {student.last_name}',
-            student.email,
-            student.gender.value,
-            student.mobile,
-            student.date_of_birth.strftime('%d %B,%Y'),
-            ', '.join(subject.value for subject in student.subjects),
-            ', '.join(hobby.value for hobby in student.hobbies),
-            student.avatar,
-            student.address,
-            f'{student.state.value} {student.city.value}',
-        )
-    )
+
+def test_register_a_student_using_simplified_form():
+
+    app.open()
+
+    app.home_page.select_elements()
+    app.forms_page.panel.select_simple_registration_form()
+    app.simple_registration_page.register(student)
+
+    app.simple_registration_page.should_be_registered(student)
